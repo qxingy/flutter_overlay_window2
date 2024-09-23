e74a420fd5db536c6178a01adf76281d7dea35e6
qxingy <1213383851@qq.com>
2024-09-24 01:02:23 +0800

    update

--- lib/flutter_overlay_window2.dart
+++ lib/flutter_overlay_window2.dart
@@ -1,4 +1,4 @@
-library flutter_overlay_window;
+library flutter_overlay_window2;
 
 export 'package:flutter_overlay_window/src/overlay_window.dart';
 export 'package:flutter_overlay_window/src/overlay_config.dart';
